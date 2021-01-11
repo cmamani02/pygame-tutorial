6909da46c043abcc0df60e254a19a7354d080770
cmamani <mamanimezacarlos@gmail.com>
2021-01-11 17:48:11 -0300

    movimiento de rectangulo sin restricciones

--- firstGame.py
+++ firstGame.py
@@ -17,6 +17,21 @@ while run:
     pygame.time.delay(100)#milliseconds
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False 
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel    
+    if keys[pygame.K_DOWN]:
+        y += vel     
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, heigth))     
+    pygame.display.update()    
 
 pygame.quit()
