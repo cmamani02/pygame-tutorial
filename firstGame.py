8d3fa21707f3acec72b31b35c7a93d6661fab0d6
cmamani <mamanimezacarlos@gmail.com>
2021-01-11 17:14:32 -0300

    creando primer ventana

--- firstGame.py
+++ firstGame.py
@@ -1,2 +1,22 @@
 import pygame
 pygame.init()
+
+win = pygame.display.set_mode((500,500))
+
+pygame.display.set_caption("First Game")
+
+x = 50
+y = 50
+width = 40
+heigth = 60
+vel = 5
+
+#main loop
+run = True
+while run:
+    pygame.time.delay(100)#milliseconds
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False 
+
+pygame.quit()
